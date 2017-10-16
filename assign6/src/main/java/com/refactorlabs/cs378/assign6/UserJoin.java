0a11099139bc1e9e619d421df263c9649c72f112
David Malone <dmalone7@cs.utexas.edu>
2017-10-16 18:28:06 -0500

    assign6 compiles with maven

--- assign6/src/main/java/com/refactorlabs/cs378/assign6/UserJoin.java
+++ assign6/src/main/java/com/refactorlabs/cs378/assign6/UserJoin.java
@@ -1,11 +1,13 @@
 package com.refactorlabs.cs378.assign6;
 
 import com.refactorlabs.cs378.utils.Utils;
+import com.refactorlabs.cs378.sessions.*;
+import org.apache.avro.Schema;
+import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapred.AvroValue;
 import org.apache.avro.mapreduce.AvroJob;
-import org.apache.avro.mapred.AvroMultipleInputs;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
-
+import org.apache.avro.mapreduce.AvroKeyValueInputFormat;
+import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -22,8 +24,11 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.StringTokenizer;
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -38,21 +43,40 @@ public class UserJoin extends Configured implements Tool {
 		private Text word = new Text();
 
 		@Override
-		public void map(LongWritable key, Text value, Context context)
+		public void map(AvroKey<CharSequence> key, AvroValue<Session> value, Context context)
 				throws IOException, InterruptedException {
+
+			Map<String, Long> map = new HashMap<String, Long>();
+			Map<CharSequence, Long> clickMap = new HashMap<CharSequence, Long>();
+
 			Session userSession = value.datum();
 
 			for (Event event : userSession.getEvents()) {
-				
+				if (map.containsKey(event.getVin().toString())) // if the key exists, don't overwrite the previous value
+					map.put(event.getVin().toString(), 0L);
+				if (event.getEventType() == EventType.CLICK) {
+					// more efficient to overwrite previous value, or check if exists?
+					// if (clickMap.containsKey(event.getVin().toString()))
+					clickMap.put(event.getEventSubtype().toString(), 1L);
+				}
+				else if (event.getEventType() == EventType.EDIT && event.getEventSubtype() == EventSubtype.CONTACT_FORM) {
+					map.put(event.getVin().toString(), 1L);
+				}
 			}
-			// setUniqueUsers(Long value)
-			// setClicks(Map<java.lang.CharSequence, Long> value)
-			// setEditContactForm(Long value)
+
+			for (String vin : map.keySet()) {
+				word.set(vin);
+				VinImpressionCounts.Builder builder = VinImpressionCounts.newBuilder();
+				builder.setUniqueUsers(1L); 			  // setUniqueUsers(Long value) set to 1 from mapper
+				builder.setClicks(clickMap); 			  // setClicks(Map<java.lang.CharSequence, Long> value)
+				builder.setEditContactForm(map.get(vin)); // setEditContactForm(Long value)
+				context.write(word, new AvroValue<VinImpressionCounts>(builder.build()));
+			}
 		}
 	}
 
 	// read vin,impression_type,count input from CSV files
-	public static class VinImpressionMapperMapper extends Mapper<LongWritable, Text, Text, AvroValue<VinImpressionCounts>> {
+	public static class VinImpressionMapper extends Mapper<LongWritable, Text, Text, AvroValue<VinImpressionCounts>> {
 
 		private Text word = new Text();
 
@@ -67,14 +91,15 @@ public class UserJoin extends Configured implements Tool {
 
 			VinImpressionCounts.Builder builder = VinImpressionCounts.newBuilder();
 			if (splitLine[1].equals("SRP"))
-				builder.setMarketplaceSrps(Long.parseLong(slitLine[2]));
+				builder.setMarketplaceSrps(Long.parseLong(splitLine[2]));
 			else
-				builder.setMarketplaceVdps(Long.parseLong(slitLine[2]));
+				builder.setMarketplaceVdps(Long.parseLong(splitLine[2]));
 			context.write(word, new AvroValue<VinImpressionCounts>(builder.build()));
 		}
 	}
 
-	public static class ReduceClass extends <Text, AvroValue<VinImpressionCounts>, Text, AvroValue<VinImpressionCounts>> {
+
+	public static class ReduceClass extends Reducer<Text, AvroValue<VinImpressionCounts>, Text, AvroValue<VinImpressionCounts>> {
 
 		@Override
 		public void reduce(Text key, Iterable<AvroValue<VinImpressionCounts>> values, Context context)
@@ -86,10 +111,10 @@ public class UserJoin extends Configured implements Tool {
 			long marketplaceVdps = 0;
 
 			for (AvroValue<VinImpressionCounts> value : values) {
-				uniqueUsers += value.getUniqueUsers();
-				editContactForm += value.getEditContactForm();
-				marketplaceSrps += value.getMarketplaceSrps();
-				marketplaceVdps += value.getMarketplaceVdps();
+				uniqueUsers += value.datum().getUniqueUsers();
+				editContactForm += value.datum().getEditContactForm();
+				marketplaceSrps += value.datum().getMarketplaceSrps();
+				marketplaceVdps += value.datum().getMarketplaceVdps();
 			}
 
 			// implement left side join, don't output if right side is empty
@@ -101,7 +126,7 @@ public class UserJoin extends Configured implements Tool {
 			builder.setEditContactForm(editContactForm);
 			builder.setMarketplaceSrps(marketplaceSrps);
 			builder.setMarketplaceVdps(marketplaceVdps);
-			context.write(word, new AvroValue<VinImpressionCounts>(builder.build()));
+			context.write(key, new AvroValue<VinImpressionCounts>(builder.build()));
 		}
 	}
 
@@ -111,36 +136,39 @@ public class UserJoin extends Configured implements Tool {
 	 */
 	public int run(String[] args) throws Exception {
 		if (args.length != 2 && args.length != 3) {
-			System.err.println("Usage: UserJoin <input path> <output path>");
+			System.err.println("Usage: UserJoin <input path> <input path> <input path> <output path>");
 			return -1;
 		}
 
 		Configuration conf = getConf();
+		// Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "UserJoin");
 		String[] appArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
 		// Identify the JAR file to replicate to all machines.
 		job.setJarByClass(UserJoin.class);
 
+		MultipleInputs.addInputPath(job, new Path(appArgs[0]), AvroKeyValueInputFormat.class, UserSessionsMapper.class);
+		MultipleInputs.addInputPath(job, new Path(appArgs[1]), TextInputFormat.class, VinImpressionMapper.class);
+		MultipleInputs.addInputPath(job, new Path(appArgs[2]), TextInputFormat.class, VinImpressionMapper.class);
+
 		// Specify the Map
 		job.setMapOutputKeyClass(Text.class);
 		AvroJob.setInputKeySchema(job, Schema.create(Schema.Type.STRING));
 		AvroJob.setInputValueSchema(job, Session.getClassSchema());
+
 		AvroJob.setMapOutputValueSchema(job, VinImpressionCounts.getClassSchema());
 
 		// Specify the Reduce
 		job.setOutputFormatClass(TextOutputFormat.class);
 		job.setReducerClass(ReduceClass.class);
 		job.setOutputKeyClass(Text.class);
-		AvroJob.setOutputValueSchema(job, WordStatisticsData.getClassSchema()); 
+		AvroJob.setOutputValueSchema(job, VinImpressionCounts.getClassSchema()); 
 
 		// Grab the input file and output directory from the command line.
 		// FileInputFormat.addInputPaths(job, appArgs[0] + "," + appArgs[1]);
 
 		// specify AvroKeyValueInputFormat as the input format
-		MultipleInputs.addInputPath(job, new Path(appArgs[0]), AvroKeyValueInputFormat.class, UserSessionsMapper.class);
-		MultipleInputs.addInputPath(job, new Path(appArgs[1]), TextInputFormat.class, VinImpressionMapper.class);
-		MultipleInputs.addInputPath(job, new Path(appArgs[2]), TextInputFormat.class, VinImpressionMapper.class);
 		FileOutputFormat.setOutputPath(job, new Path(appArgs[3]));
 
 		// Initiate the map-reduce job, and wait for completion.
@@ -156,7 +184,7 @@ public class UserJoin extends Configured implements Tool {
 	 */
 	public static void main(String[] args) throws Exception {
 		Utils.printClassPath();
-		int res = ToolRunner.run(new WordStatisticsA(), args);
+		int res = ToolRunner.run(new UserJoin(), args);
 		System.exit(res);
 	}
 
--- assign6/src/main/java/com/refactorlabs/cs378/utils/Utils.java
+++ assign6/src/main/java/com/refactorlabs/cs378/utils/Utils.java
@@ -2,14 +2,14 @@ package com.refactorlabs.cs378.utils;
 
 import org.apache.hadoop.io.LongWritable;
 
+import com.refactorlabs.cs378.sessions.*;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
+import java.util.ArrayList;
 
-/**
- * Created by davidfranke on 10/5/14.
- */
 public class Utils {
-
 	// Not instantiable
 	private Utils() {}
 
@@ -37,4 +37,223 @@ public class Utils {
 		System.out.flush();
 	}
 
+	public static Session.Builder createSession(String line) {
+		Utils util = new Utils();
+
+		String[] split = line.split("\t");
+		Event.Builder eventBuilder = util.createEvent(split);
+
+		List<Event> eventList = new ArrayList<>();
+		eventList.add(eventBuilder.build());
+
+		Session.Builder builder = Session.newBuilder();
+		builder.setUserId(split[0]);
+		builder.setEvents(eventList);
+
+		return builder;
+	}
+
+	private Event.Builder createEvent(String[] splitLine) {
+		// Session.Builder builder = Session.newBuilder();
+		Event.Builder event = Event.newBuilder();
+		eventTypeHandler(event, splitLine[1]);
+		eventSubtypeHandler(event, splitLine[1]);
+		eventTimeHandler(event, splitLine[2]);
+		cityHandler(event, splitLine[3]);
+		vinHandler(event, splitLine[4]);
+		conditionHandler(event, splitLine[5]);
+		yearHandler(event, splitLine[6]);
+		makeHandler(event, splitLine[7]);
+		modelHandler(event, splitLine[8]);
+		trimHandler(event, splitLine[9]);
+		bodyStyleHandler(event, splitLine[10]);
+		cabStyleHandler(event, splitLine[11]);
+		priceHandler(event, splitLine[12]);
+		mileageHandler(event, splitLine[13]);
+		freeCarfaxReportHandler(event, splitLine[14]);
+		featuresHandler(event, splitLine[15]);
+
+		return event;
+	}
+
+	private void eventTypeHandler(Event.Builder event, String eventType) {
+		String[] eventSplit = eventType.split(" ");
+		switch(eventSplit[0].toUpperCase()) {
+			case "CHANGE":
+				event.setEventType(EventType.CHANGE);
+				break;
+			case "CLICK":
+				event.setEventType(EventType.CLICK);
+				break;
+			case "DISPLAY":
+				event.setEventType(EventType.DISPLAY);
+				break;
+			case "EDIT": 
+				event.setEventType(EventType.EDIT);
+				break;
+			case "SHOW":
+				event.setEventType(EventType.SHOW);
+				break;
+			case "SUBMIT":
+				event.setEventType(EventType.SUBMIT);
+				break;
+			case "VISIT":
+				event.setEventType(EventType.VISIT);
+				break;
+			default: 
+				event.setEventType(EventType.DISPLAY);
+		}
+	}
+	private void eventSubtypeHandler(Event.Builder event, String eventSubtype) {
+		String[] eventSplit = eventSubtype.split(" ");
+
+		if (eventSplit.length > 2)
+			eventSubtype = eventSplit[1] + " " + eventSplit[2];
+		else
+			eventSubtype = eventSplit[1];
+
+		switch(eventSubtype.toUpperCase()) {
+			case "CONTACT FORM":
+				event.setEventSubtype(EventSubtype.CONTACT_FORM);
+				break;
+			case "CONTACT BANNER":
+				event.setEventSubtype(EventSubtype.CONTACT_BANNER);
+				break;
+			case "CONTACT BUTTON":
+				event.setEventSubtype(EventSubtype.CONTACT_BUTTON);
+				break;
+			case "DEALER PHONE":
+				event.setEventSubtype(EventSubtype.DEALER_PHONE);
+				break;
+			case "FEATURES":
+				event.setEventSubtype(EventSubtype.FEATURES);
+				break;
+			case "GET DIRECTIONS":
+				event.setEventSubtype(EventSubtype.GET_DIRECTIONS);
+				break;
+			case "VEHICLE HISTORY":
+				event.setEventSubtype(EventSubtype.VEHICLE_HISTORY);
+				break;
+			case "ALTERNATIVE":
+				event.setEventSubtype(EventSubtype.ALTERNATIVE);
+				break;
+			case "BADGE DETAIL": 
+				event.setEventSubtype(EventSubtype.BADGE_DETAIL);
+				break;
+			case "PHOTO MODAL":
+				event.setEventSubtype(EventSubtype.PHOTO_MODAL);
+				break;
+			case "BADGES":
+				event.setEventSubtype(EventSubtype.BADGES);
+				break;
+			case "MARKET REPORT":
+				event.setEventSubtype(EventSubtype.MARKET_REPORT);
+				break;
+			default:
+				event.setEventSubtype(EventSubtype.FEATURES);
+		}
+	}
+	private void eventTimeHandler(Event.Builder event, String time) {
+		event.setEventTime(time);
+	}
+	private void cityHandler(Event.Builder event, String city) {
+		event.setCity(city);
+	}
+	private void vinHandler(Event.Builder event, String vin) {
+		event.setVin(vin);
+	}
+	private void conditionHandler(Event.Builder event, String vehicleCondition) {
+		Condition condition = Condition.USED;
+		if (vehicleCondition.toUpperCase().equals("NEW"))
+			condition = Condition.NEW;
+		event.setCondition(condition);
+	}
+	private void yearHandler(Event.Builder event, String year) {
+		event.setYear(Integer.parseInt(year));
+	}
+	private void makeHandler(Event.Builder event, String make) {
+		event.setMake(make);
+	}
+	private void modelHandler(Event.Builder event, String model) {
+		event.setModel(model);
+	}
+	private void trimHandler(Event.Builder event, String trim) {
+		if (trim.equals(""))
+			event.setTrim(null);
+		else
+			event.setTrim(trim);
+	}
+	private void bodyStyleHandler(Event.Builder event, String bodyStyle) {
+		switch(bodyStyle.toUpperCase()) {
+			case "CASSIS":
+				event.setBodyStyle(BodyStyle.CASSIS);
+				break;
+			case "CONVERTIBLE":
+				event.setBodyStyle(BodyStyle.CONVERTIBLE);
+				break;
+			case "COUPE":
+				event.setBodyStyle(BodyStyle.COUPE);
+				break;
+			case "HATCHBACK":
+				event.setBodyStyle(BodyStyle.HATCHBACK);
+				break;
+			case "MINIVAN":
+				event.setBodyStyle(BodyStyle.MINIVAN);
+				break;
+			case "PICKUP":
+				event.setBodyStyle(BodyStyle.PICKUP);
+				break;
+			case "SUV":
+				event.setBodyStyle(BodyStyle.SUV);
+				break;
+			case "SEDAN":
+				event.setBodyStyle(BodyStyle.SEDAN);
+				break;
+			case "VAN":
+				event.setBodyStyle(BodyStyle.VAN);
+				break;
+			case "WAGON":
+				event.setBodyStyle(BodyStyle.WAGON);
+				break;
+			default:
+				event.setBodyStyle(BodyStyle.COUPE);
+		}
+	}
+	private void cabStyleHandler(Event.Builder event, String cabStyle) {
+		switch(cabStyle.toUpperCase()) {
+			case "CREW CAB": 
+				event.setCabStyle(CabStyle.CREW);
+				break;
+			case "EXTENDED CAB": 
+				event.setCabStyle(CabStyle.EXTENDED);
+				break;
+			case "REGULAR CAB": 
+				event.setCabStyle(CabStyle.REGULAR);
+				break;
+			default:
+				event.setCabStyle(null);
+		}
+	}
+	private void priceHandler(Event.Builder event, String price) {
+		event.setPrice(Double.parseDouble(price));
+	}
+	private void mileageHandler(Event.Builder event, String mileage) {
+		event.setMileage(Long.parseLong(mileage));
+	}
+	private void freeCarfaxReportHandler(Event.Builder event, String freeCarfaxReport) {
+		boolean free = false;
+		if (freeCarfaxReport.equals("t"))
+			free = true;
+		event.setFreeCarfaxReport(free);
+	}
+	private void featuresHandler(Event.Builder event, String features) {
+		List<CharSequence> featureList = new ArrayList<>();
+		String[] splitFeatures = features.split(":");
+		for (String feature : splitFeatures) {
+			if (feature.equals("null"))
+				continue;
+			featureList.add(feature);
+		}
+		event.setFeatures(featureList);
+	}
 }
